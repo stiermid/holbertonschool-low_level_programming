14ca7f5eaf10e210721ffd1b065b4c6a5bbcb0c9
Agil Mammadov <mammadovthenull@gmail.com>
2025-11-26 10:30:34 +0400

    fix(hello_world): task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - runs the program
+ * Return: always 0
+ */
 int main(void)
 {
+	/*
+	 * MAIN()
+	 */
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
