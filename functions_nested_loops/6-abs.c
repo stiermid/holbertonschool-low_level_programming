5216b59a5087543f05cf338281a0b71c46c33d85
Agil Mammadov <mammadovthenull@gmail.com>
2025-12-02 11:00:13 +0400

    feat(fun): task6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - returns absolute value of an integer
+ * @n: integer to process
+ *
+ * Return: absolute value of the given int
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
+
