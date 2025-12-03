1f1599e0ca2f7488ff7a48bf80556907cabd5864
Agil Mammadov <mammadovthenull@gmail.com>
2025-12-03 09:29:39 +0400

    feat(more_fun): task2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: multiplication of these integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
