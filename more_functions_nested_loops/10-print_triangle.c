ef1b9efc06d5ecf84e9bfeb19272a620bdbed8cc
Agil Mammadov <mammadovthenull@gmail.com>
2025-12-03 21:24:37 +0400

    feat(more_fun): add task10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - draws a square
+ * @size: the size
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j;
+	
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 0; j < size - i; j++)
+			_putchar(' ');
+
+		for (j = 0; j < i; j++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
+
