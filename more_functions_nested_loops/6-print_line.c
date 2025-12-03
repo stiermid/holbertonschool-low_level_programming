d73c6686f5eeb7bfc4e85a21d367959593e5b23e
Agil Mammadov <mammadovthenull@gmail.com>
2025-12-03 10:16:14 +0400

    feat(more_fun): task6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: the length
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
+
