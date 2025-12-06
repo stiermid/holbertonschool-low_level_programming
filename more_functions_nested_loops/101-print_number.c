1e3dd4a6bd582b84047924e5c8d4f2c85dd1b76f
Agil Mammadov <mammadovthenull@gmail.com>
2025-12-06 23:13:45 +0400

    fix(more_fun): task12
    * betty
    * INT_MIN

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,29 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers
+ * print_number - prints number
  * @n: number to print
  *
  * Return: void
  */
 void print_number(int n)
 {
-	int temp, divisor;
+	int temp, divisor, int_min;
+
+	int_min = 0;
 
 	if (n < 0)
 	{
 		_putchar('-');
+
+		if (n == -2147483648)
+		{
+			int_min = 1;
+			n++;
+		}
+
 		n = -n;
 	}
-
 	if (n == 0)
 	{
 		_putchar('0');
@@ -32,6 +40,11 @@ void print_number(int n)
 
 	while (divisor >= 1)
 	{
+		if (int_min && divisor == 1)
+		{
+			_putchar('8');
+			return;
+		}
 		_putchar(n / divisor % 10 + '0');
 		divisor /= 10;
 	}
