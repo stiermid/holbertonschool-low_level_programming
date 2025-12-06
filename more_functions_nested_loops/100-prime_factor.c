0ca2894df3b3977ce1908d8385253620eecba718
Agil Mammadov <mammadovthenull@gmail.com>
2025-12-06 21:51:33 +0400

    fix(more_fun): task11

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -8,26 +8,25 @@
 int main(void)
 {
 	long n, i;
-	long primest;
 
 	n = 612852475143;
-	i = 1;
 
 	printf("%ld\n", n);
 
 
-	while (i < n)
+	for(i = 2; i <= n; i++)
 	{
-		if (n % i == 0)
+		while (n % i == 0)
 		{
 			n /= i;
-			primest = i;
+			if (n == 1)	
+			{
+				printf("%ld\n", i);			
+				return (0);
+			}
 		}
-
-		i++;
 	}
 
-	printf("%ld\n", primest);
 	return (0);
 }
 
