ebf8d307ccd591c798748eca2701da3667382ee2
Agil Mammadov <mammadovthenull@gmail.com>
2025-12-08 11:22:02 +0400

    feat(p_arr_str): task4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: a string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	while (*s)
+	{
+		s++;
+	}
+
+	do {
+		s--;
+		_putchar(*s);
+	} while (*s);
+
+	_putchar('\n');
+}
+
