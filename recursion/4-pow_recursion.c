82704dfbbbbc372081d5e91bd7f187e99c79f4d1
Agil Mammadov <mammadovthenull@gmail.com>
2025-12-16 17:40:10 +0400

    feat(recursion): task4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - x rasied to the power of y
+ * @x: int
+ * @y: int
+ *
+ * Return: -1 if y < 0, otherwise power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
+
