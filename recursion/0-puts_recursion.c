e0580c59ffe582e8237fdbcf98225e2ccb1363cf
Agil Mammadov <mammadovthenull@gmail.com>
2025-12-16 09:21:24 +0400

    feat(recursion): task0
    * Add _putchar.c
    * Initialize main.h

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: str
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+		_putchar('\n');
+
+}
+
