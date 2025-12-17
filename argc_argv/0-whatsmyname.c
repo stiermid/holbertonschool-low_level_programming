9974f41120987c4f8c6eff757f9a03dc211b208b
Agil Mammadov <mammadovthenull@gmail.com>
2025-12-17 10:05:19 +0400

    feat(argc_argv): task0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - run
+ * @argc: arg count
+ * @argv: args
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
