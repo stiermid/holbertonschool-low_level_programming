26cfbb493cc531e804c329a50e5e1c42510ca95f
Agil Mammadov <mammadovthenull@gmail.com>
2025-12-22 22:41:28 +0400

    feat(malloc_free): task0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of char
+ * @size: size of the array
+ * @c: char to fill
+ *
+ * Return: NULL if size = 0 or it fails, otherwise a pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	arr = malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+
+	return (arr);
+}
+
