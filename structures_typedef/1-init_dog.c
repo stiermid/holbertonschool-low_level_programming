b42d995b5330a50570ef7ac81b1227382ae5281b
Agil Mammadov <mammadovthenull@gmail.com>
2025-12-30 12:25:46 +0400

    fix(struct_typedef): task1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - initializes dog
@@ -11,8 +12,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
 
