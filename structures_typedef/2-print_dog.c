860db6572fac13ad0d50da6475c6652bd5833a8b
Agil Mammadov <mammadovthenull@gmail.com>
2025-12-30 13:34:47 +0400

    fix(struct_typedef): task2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -18,7 +18,7 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 
-	printf("Age: %.1f\n", d->age);
+	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
