c4315db252e241a9545baf4077fc7ff75d834d4a
Agil Mammadov <mammadovthenull@gmail.com>
2025-12-30 12:18:39 +0400

    feat(struct_typedef): task1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes dog
+ * @d: dog struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+
