51e15a6dc93c916e54db70316bd0798e7797b267
Agil Mammadov <mammadovthenull@gmail.com>
2026-01-01 11:16:50 +0400

    feat(func_pointers): task0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ * @name: name to print
+ * @f: print function
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
