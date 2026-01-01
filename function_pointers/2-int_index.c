0a1ede45f3f8882253c924aeca28499307bce494
Agil Mammadov <mammadovthenull@gmail.com>
2026-01-01 11:33:04 +0400

    feat(func_pointers): task2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - iterates
+ * @array: array
+ * @size: size of the array
+ * @cmp: compare function
+ *
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
+
