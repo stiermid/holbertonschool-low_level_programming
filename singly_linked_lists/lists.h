87fdd2109dc5cacf869bc04b143ce29cb3a78e40
Agil Mammadov <mammadovthenull@gmail.com>
2026-01-16 22:27:04 +0400

    feat(singly_linked_lists): task0

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -17,5 +20,6 @@ typedef struct list_s
 } list_t;
 
 int _putchar(char c);
+size_t print_list(const list_t *h);
 
 #endif
