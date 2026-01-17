c8243f9fff640e05f2f70e077e6725a248404d0d
Agil Mammadov <mammadovthenull@gmail.com>
2026-01-17 19:04:11 +0400

    feat(doubly_linked_lists): task5

---
+++ doubly_linked_lists/5-get_dnoteint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Returns the nth node of a dlistint_t linked list
+ * @head: pointer to the head of the list
+ * @index: the index of the node to locate, starting from 0
+ *
+ * Return: the address of the located node, or NULL if it does not exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head != NULL)
+	{
+		if (i == index)
+			return (head);
+
+		head = head->next;
+		i++;
+	}
+
+	return (NULL);
+}
+
