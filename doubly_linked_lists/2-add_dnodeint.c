74848aaf31dbf56c74619190d8c682e5a727cf3a
Agil Mammadov <mammadovthenull@gmail.com>
2026-01-17 15:56:04 +0400

    feat(doubly_linked_lists): task2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add node to the begining of the list
+ * @head: a double pointer to the head of the list
+ * @n: the integer to store in the new node
+ *
+ * Return: the address of the new element, NULL on failure
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
+	*head = new_node;
+	return (new_node);
+}
+
