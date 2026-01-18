0192365e746a8743cacfb2f87879591c436f3a1f
Agil Mammadov <mammadovthenull@gmail.com>
2026-01-18 21:04:18 +0400

    fix(doubly_linked_lists): task11

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -3,63 +3,52 @@
 #include <string.h>
 
 /**
- * main - Keygen for crackme5
- * @argc: Number of arguments
- * @argv: Arguments passed (username)
+ * main - Generates and prints passwords for the crackme5 executable.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: 0 on success, 1 on error
+ * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	unsigned int i, sum;
-	size_t len;
-	char *user;
-	char key[7];
-	char *lookup = "A-CHRDw87lNS0E9B2TibgpnY70oDsUbhS9StWDYG6t899alvLzablvLzablvLz";
+	char password[7], *codex;
+	int len = strlen(argv[1]), i, tmp;
 
-	lookup = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-	if (argc != 2)
-		return (1);
+	tmp = (len ^ 59) & 63;
+	password[0] = codex[tmp];
 
-	user = argv[1];
-	len = strlen(user);
-
-	sum = 0;
+	tmp = 0;
 	for (i = 0; i < len; i++)
-		sum += user[i];
-	key[0] = lookup[(sum ^ 59) & 63];
+		tmp += argv[1][i];
+	password[1] = codex[(tmp ^ 79) & 63];
 
+	tmp = 1;
 	for (i = 0; i < len; i++)
-		sum *= user[i];
-	key[1] = lookup[(sum ^ 79) & 63];
+		tmp *= argv[1][i];
+	password[2] = codex[(tmp ^ 85) & 63];
 
-	sum = user[0];
+	tmp = 0;
 	for (i = 0; i < len; i++)
-		if (user[i] > (char)sum)
-			sum = user[i];
-	srand(sum ^ 14);
-	key[2] = lookup[rand() & 63];
+	{
+		if (argv[1][i] > tmp)
+			tmp = argv[1][i];
+	}
+	srand(tmp ^ 14);
+	password[3] = codex[rand() & 63];
 
-	sum = 0;
+	tmp = 0;
 	for (i = 0; i < len; i++)
-		sum += (user[i] * user[i]);
-	key[3] = lookup[(sum ^ 239) & 63];
+		tmp += (argv[1][i] * argv[1][i]);
+	password[4] = codex[(tmp ^ 239) & 63];
 
-	sum = 0;
-	for (i = 0; i < len; i++)
-		sum = rand();
-	key[4] = lookup[(sum ^ 155) & 63];
+	for (i = 0; i < argv[1][0]; i++)
+		tmp = rand();
+	password[5] = codex[(tmp ^ 229) & 63];
 
-	sum = 0;
-	for (i = 0; i < (unsigned int)user[0]; i++)
-		sum = rand();
-	key[5] = lookup[(sum ^ 255) & 63];
-
-	key[6] = '\0';
-	printf("%s", key);
-
+	password[6] = '\0';
+	printf("%s", password);
 	return (0);
 }
-
 
