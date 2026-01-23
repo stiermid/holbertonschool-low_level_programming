d82a89495d412d7db08c38c704475eadb670f789
Agil Mammadov <mammadovthenull@gmail.com>
2026-01-23 23:09:35 +0400

    feat(hash_map): task0

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,6 +1,9 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -29,6 +32,8 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
 
 #endif
 
