80f1c458c2cd70de89a833368270ad3ede6885c9
Agil Mammadov <mammadovthenull@gmail.com>
2026-01-23 23:21:20 +0400

    fix(hash_map): task1

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,12 +9,29 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i;
 
+	if (size == 0)
+		return (NULL);
+
+	/* allocate memory for table structure */
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
+
+	/* allocate memory for the array of pointers */
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	/* init the size and set all array elements to NULL */
 	table->size = size;
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
