d2b2ea6066d436ab6de765897770855cca0bdad3
Agil Mammadov <mammadovthenull@gmail.com>
2026-01-23 12:14:54 +0400

    fix(file_io): task3
    * error message

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -32,7 +32,7 @@ int main(int argc, char **argv)
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
 		exit(99);
 	}
